13391cc167de6ebcb6ddbae47fdcfee540ceec24
Cesar Oliva <83353946+CesarOliva@users.noreply.github.com>
2025-04-01 17:52:53 -0600

    Crete main.js
    Adding funtionality of the menu links

---
+++ main.js
@@ -0,0 +1,26 @@
+var queEs = document.getElementById('que-es-a');
+var mision = document.getElementById('mision-vision-a');
+var valores = document.getElementById('valores-a');
+var integrantes = document.getElementById('integrantes-a');
+var colaboradores = document.getElementById('colaboradores-a');
+
+queEs.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+);
+mision.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+);
+valores.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+);
+integrantes.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+);
+colaboradores.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+);
